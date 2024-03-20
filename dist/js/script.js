e0a73c21bee287f23e2589f80358c12b9f7f89d3
MIKHAIL PANTELEEV <mihanpan1996@mail.ru>
2024-03-20 04:28:02 +0300

    добавлен рандомный разворот 3ех карточек в блоке "что я использую в работе"

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,3 +16,35 @@ const counters = document.querySelectorAll(".skills__ratings-counter"),
 counters.forEach((item, i) => {
   lines[i].style.width = item.innerHTML;
 });
+
+//рандомный разворот 3ех карточек
+const items = document.querySelectorAll(".skills__item");
+let indexes = Array.from({ length: items.length }, (_, i) => i);
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+shuffle(indexes);
+
+let currentIndex1 = 0;
+let currentIndex2 = 1;
+let currentIndex3 = 2;
+
+function rotateCards() {
+  items.forEach((item) => item.classList.remove("rotate-360"));
+
+  items[indexes[currentIndex1]].classList.add("rotate-360");
+  items[indexes[currentIndex2]].classList.add("rotate-360");
+  items[indexes[currentIndex3]].classList.add("rotate-360");
+
+  currentIndex1 = (currentIndex1 + 3) % items.length;
+  currentIndex2 = (currentIndex2 + 3) % items.length;
+  currentIndex3 = (currentIndex3 + 3) % items.length;
+}
+
+setInterval(rotateCards, 7000); // Поворачиваем каждые 7 секунд (7000 миллисекунд)
